08c9860e8db2752b512ffcb92a8a5f04cf06fdfc
unknown <dani99mr31@gmail.com>
2022-12-16 00:44:19 +0100

    Created Database and blockchain iterator over the data stored in the database

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,13 +1,10 @@
 package blockchain
 
 import (
-	"fmt"
-	"strconv"
+	"bytes"
+	"encoding/gob"
+	"log"
 )
-
-type Blockchain struct {
-	Blocks []*Block
-}
 
 type Block struct {
 	Hash     []byte // hash of the block
@@ -28,29 +25,34 @@ func createBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
-func (chain *Blockchain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	newBlock := createBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, newBlock)
-}
-
 // The Genesis block is the first block of a Blockchain
 func genesis() *Block {
 	return createBlock("Genesis", []byte{})
 }
 
-func InitBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{genesis()}}
+func (block *Block) Serialize() []byte {
+	var res bytes.Buffer
+	encoder := gob.NewEncoder(&res)
+
+	err := encoder.Encode(block)
+
+	HandleError(err)
+
+	return res.Bytes()
 }
 
-func (chain *Blockchain) DisplayData() {
-	for _, block := range chain.Blocks {
-		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
-		fmt.Printf("Data in block: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
+func Deserialize(data []byte) *Block {
+	var block Block
+	decoder := gob.NewDecoder(bytes.NewReader(data))
 
-		pow := NewProof(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
+	err := decoder.Decode(&block)
+
+	HandleError(err)
+	return &block
+}
+
+func HandleError(err error) {
+	if err != nil {
+		log.Panic(err)
 	}
 }
